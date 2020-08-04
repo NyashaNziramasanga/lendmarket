5268ffb8cea5c810bcc3ee61050d8c15b91a86dc
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-08-04 23:00:13 +0930

    added icons

--- layouts/main.tsx
+++ layouts/main.tsx
@@ -1,60 +1,66 @@
-import dynamic from 'next/dynamic'
-import Link from 'next/link'
+import dynamic from 'next/dynamic';
+import Link from 'next/link';
 
 import {
-  SmileOutlined,
-  SettingOutlined,
-  PlaySquareOutlined,
-} from '@ant-design/icons'
+  UserOutlined,
+  HomeOutlined,
+  ProfileOutlined,
+  SolutionOutlined,
+} from '@ant-design/icons';
 
-import { Route, MenuDataItem } from '@ant-design/pro-layout/lib/typings'
-import { SiderMenuProps } from '@ant-design/pro-layout/lib/SiderMenu/SiderMenu'
+import { Route, MenuDataItem } from '@ant-design/pro-layout/lib/typings';
+import { SiderMenuProps } from '@ant-design/pro-layout/lib/SiderMenu/SiderMenu';
 
 const ProLayout = dynamic(() => import('@ant-design/pro-layout'), {
   ssr: false,
-})
+});
 
 const ROUTES: Route = {
   path: '/',
   routes: [
     {
       path: '/',
-      name: 'Welcome',
-      icon: <SmileOutlined />,
+      name: 'Investor',
+      icon: <UserOutlined />,
       routes: [
         {
-          path: '/welcome',
-          name: 'Account Settings',
-          icon: <SettingOutlined />,
+          path: '/marketplace',
+          name: 'Marketplace',
+          icon: <HomeOutlined />,
+        },
+        {
+          path: '/loan-exchange',
+          name: 'Loan Exchange',
+          icon: <ProfileOutlined />,
         },
       ],
     },
     {
-      path: '/example',
-      name: 'Example Page',
-      icon: <PlaySquareOutlined />,
+      path: '/borrower',
+      name: 'borrower',
+      icon: <SolutionOutlined />,
     },
   ],
-}
+};
 
 const menuHeaderRender = (
   logoDom: React.ReactNode,
   titleDom: React.ReactNode,
-  props: SiderMenuProps
+  props: SiderMenuProps,
 ) => (
-  <Link href="/">
+  <Link href='/'>
     <a>
       {logoDom}
       {!props?.collapsed && titleDom}
     </a>
   </Link>
-)
+);
 
 const menuItemRender = (options: MenuDataItem, element: React.ReactNode) => (
   <Link href={options.path}>
     <a>{element}</a>
   </Link>
-)
+);
 
 export default function Main({ children }) {
   return (
@@ -66,5 +72,5 @@ export default function Main({ children }) {
     >
       {children}
     </ProLayout>
-  )
+  );
 }
