e3ed794692e91cccb2a54a7217e1b8caac1b6d53
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-08-05 14:58:49 +0930

    Added tabs and borrower

---
+++ pages/borrower.tsx
@@ -0,0 +1,5 @@
+import MainLayout from '../layouts/main';
+
+export default function Borrower() {
+  return <MainLayout>Borrower</MainLayout>;
+}
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,6 +1,8 @@
 import MainLayout from '../layouts/main';
-import { Card, Breadcrumb } from 'antd';
+import { Card, Breadcrumb, Tabs } from 'antd';
 import { HomeOutlined, UserOutlined } from '@ant-design/icons';
+
+const { TabPane } = Tabs;
 
 export default function Dashboard() {
   return (
@@ -22,6 +24,34 @@ export default function Dashboard() {
         <h2>$10,000.00</h2>
         <p>Total Current Investments:</p>
         <h2>$5,000.00</h2>
+      </Card>
+      <br />
+      <Card title='Other' type='inner' style={{ width: '100%' }}>
+        <Tabs
+          defaultActiveKey='1'
+          size={'small'}
+          type='line'
+          style={{ marginBottom: 32 }}
+        >
+          <TabPane tab='Current Bids' key='1'>
+            <p>Content of tab 1</p>
+          </TabPane>
+          <TabPane tab='Current Investments' key='2'>
+            <p>Content of tab 2</p>
+          </TabPane>
+          <TabPane tab='Your Loan Parts for sale' key='3'>
+            <p>Content of tab 3</p>
+          </TabPane>
+          <TabPane tab='Loan Parts Returns' key='4'>
+            <p>Content of tab 4</p>
+          </TabPane>
+          <TabPane tab='Your Investor Statements' key='5'>
+            <p>Content of tab 5</p>
+          </TabPane>
+          <TabPane tab='My Bids' key='6'>
+            <p>Content of tab 6</p>
+          </TabPane>
+        </Tabs>
       </Card>
     </MainLayout>
   );
