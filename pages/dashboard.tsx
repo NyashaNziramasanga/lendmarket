7557e7d9edf2e0f6fe552165f0ec25c4b7360254
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-08-05 11:00:35 +0930

    Added breadcrumb

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,10 +1,22 @@
 import MainLayout from '../layouts/main';
-import { Card } from 'antd';
+import { Card, Breadcrumb } from 'antd';
+import { HomeOutlined, UserOutlined } from '@ant-design/icons';
 
 export default function Dashboard() {
   return (
     <MainLayout>
-      Dashboard
+      {/*TODO: Make breadcrumb dynamic */}
+      <Breadcrumb>
+        <Breadcrumb.Item href=''>
+          <HomeOutlined />
+        </Breadcrumb.Item>
+        <Breadcrumb.Item href=''>
+          <UserOutlined />
+          <span>Investor</span>
+        </Breadcrumb.Item>
+        <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
+      </Breadcrumb>
+
       <Card title='Lendmarket Investments' type='inner' style={{ width: 300 }}>
         <p>Available for Investment:</p>
         <h2>$10,000.00</h2>
