9e890a11563205fb95479cfcbbf78eef33a37b0c
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-08-05 10:56:07 +0930

    Added a card to the dashboard

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,5 +1,16 @@
 import MainLayout from '../layouts/main';
+import { Card } from 'antd';
 
 export default function Dashboard() {
-  return <MainLayout>Dashboard</MainLayout>;
+  return (
+    <MainLayout>
+      Dashboard
+      <Card title='Lendmarket Investments' type='inner' style={{ width: 300 }}>
+        <p>Available for Investment:</p>
+        <h2>$10,000.00</h2>
+        <p>Total Current Investments:</p>
+        <h2>$5,000.00</h2>
+      </Card>
+    </MainLayout>
+  );
 }
