f4d5d9046a6176a998079f25c72370ea746a6085
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-08-08 01:57:11 +0930

    Refactored Table

--- pages/marketplace.tsx
+++ pages/marketplace.tsx
@@ -1,6 +1,8 @@
 import MainLayout from '../layouts/main';
 import { Table, Button, Progress, Avatar } from 'antd';
 
+const { Column } = Table;
+
 export default function Marketplace() {
   const data = [
     {
@@ -14,67 +16,47 @@ export default function Marketplace() {
     },
   ];
 
-  const columns = [
-    {
-      title: '',
-      key: 'image',
-      render: (record) => (
-        // TODO: if there is an image reveal else use initials
-        <Avatar
-          shape='square'
-          size='large'
-          style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}
-          // src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
-        >
-          {record.name}
-        </Avatar>
-      ),
-    },
-    {
-      title: 'Name',
-      dataIndex: 'name',
-      key: 'name',
-    },
-    {
-      title: 'Risk',
-      dataIndex: 'risk',
-      key: 'risk',
-    },
-    {
-      title: 'Amount',
-      dataIndex: 'amount',
-      key: 'amount',
-    },
-    {
-      title: 'Term (Months)',
-      dataIndex: 'term',
-      key: 'term',
-    },
-    {
-      title: 'Rate',
-      dataIndex: 'rate',
-      key: 'rate',
-    },
-    {
-      title: 'Progress',
-      key: 'progress',
-      render: (record) => (
-        <Progress
-          percent={record.progress}
-          size='small'
-          strokeColor={{ from: '#52c41a', to: '#52c41a' }}
-        />
-      ),
-    },
-    {
-      title: '',
-      key: 'action',
-      render: () => <Button>View</Button>,
-    },
-  ];
   return (
     <MainLayout>
-      <Table columns={columns} dataSource={data} />
+      <Table dataSource={data}>
+        <Column
+          title=''
+          dataIndex='image'
+          key='image'
+          render={(text, record: any) => (
+            // TODO: if there is an image reveal else use initials
+            <Avatar
+              shape='square'
+              size='large'
+              style={{ color: '#f56a00', backgroundColor: '#fde3cf' }}
+              // src='https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png'
+            >
+              {record.name}
+            </Avatar>
+          )}
+        />
+        <Column title='Name' dataIndex='name' key='name' />
+        <Column title='Risk' dataIndex='risk' key='risk' />
+        <Column title='Amount' dataIndex='amount' key='amount' />
+        <Column title='Term (Months)' dataIndex='term' key='term' />
+        <Column title='Rate' dataIndex='rate' key='rate' />
+        <Column
+          title='Progress'
+          dataIndex='progress'
+          render={(text, record: any) => (
+            <Progress
+              percent={record.progress}
+              size='small'
+              strokeColor={{ from: '#52c41a', to: '#52c41a' }}
+            />
+          )}
+        />
+        <Column
+          title=''
+          dataIndex='action'
+          render={() => <Button>View</Button>}
+        />
+      </Table>
     </MainLayout>
   );
 }
