f17c317bb458649fb0fac0f4222cb03abc8c12bc
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-08-09 02:08:15 +0930

    Updated index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,52 @@
 import MainLayout from '../layouts/main';
+import Link from 'next/link';
+import { Card, Row, Col } from 'antd';
 
 export default function Home() {
-  return <MainLayout>Welcome!</MainLayout>;
+  const adStyle: any = {
+    textAlign: 'center',
+    width: '100%',
+    background: 'white',
+    height: '300px',
+  };
+  const style: any = {
+    textAlign: 'center',
+    width: '100%',
+    background: 'white',
+  };
+  return (
+    <MainLayout>
+      <Row>
+        <Col span={24}>
+          <Card.Grid style={adStyle}>Ad</Card.Grid>
+        </Col>
+      </Row>
+      <br />
+
+      <Row>
+        <Col span={12}>
+          {/* TODO: Create Invest and borrow application form */}
+          <Link href='#'>
+            <Card.Grid style={style}>
+              <h3>Invest</h3>
+            </Card.Grid>
+          </Link>
+        </Col>
+        <Col span={12}>
+          <Link href='#'>
+            <Card.Grid style={style}>
+              <h3>Borrow</h3>
+            </Card.Grid>
+          </Link>
+        </Col>
+      </Row>
+
+      <br />
+      <Row>
+        <Col span={24}>
+          <Card.Grid style={adStyle}>How it works</Card.Grid>
+        </Col>
+      </Row>
+    </MainLayout>
+  );
 }
