4cc1fb8aa0f4350e4f228e5b9f1650a9ebaeeac6
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-08-16 00:20:40 +0930

    Update [org].tsx

--- pages/marketplace/[org].tsx
+++ pages/marketplace/[org].tsx
@@ -1,5 +1,32 @@
 import MainLayout from '../../layouts/main';
+import { Card, Tabs } from 'antd';
+
+const { TabPane } = Tabs;
 
 export default function org() {
-  return <MainLayout>Marketplace org</MainLayout>;
+  return (
+    <MainLayout>
+      <Card title='Org Name'>
+        <Tabs
+          defaultActiveKey='1'
+          size={'large'}
+          type='line'
+          style={{ marginBottom: 32 }}
+        >
+          <TabPane tab='About' key='1'>
+            <p>Content of tab 1</p>
+          </TabPane>
+          <TabPane tab='Current Bids' key='2'>
+            <p>Content of tab 2</p>
+          </TabPane>
+          <TabPane tab='Discussion' key='3'>
+            <p>Content of tab 3</p>
+          </TabPane>
+          <TabPane tab='Credit Info' key='4'>
+            <p>Content of tab 4</p>
+          </TabPane>
+        </Tabs>
+      </Card>
+    </MainLayout>
+  );
 }
