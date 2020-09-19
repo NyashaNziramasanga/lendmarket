a210ca8e17a0e275e7eaa52f612b9f9faf189b62
Nyasha Nziramasanga <nash@billowsoftware.com.au>
2020-09-19 21:48:02 +0930

    update: added borrower section

--- pages/borrower.tsx
+++ pages/borrower.tsx
@@ -1,5 +1,23 @@
 import MainLayout from '../layouts/main';
+import { Tabs, Card } from 'antd';
+const { TabPane } = Tabs;
 
 export default function Borrower() {
-  return <MainLayout>Borrower</MainLayout>;
+  return (
+    <MainLayout>
+      <Card title='Borrower' type='inner' style={{ width: '100%' }}>
+        <Tabs
+          defaultActiveKey='1'
+          size={'small'}
+          type='line'
+          style={{ marginBottom: 32 }}
+        >
+          <TabPane tab='Loans' key='1'></TabPane>
+          <TabPane tab='Repayment Schedule' key='2'></TabPane>
+          <TabPane tab='Statements' key='3'></TabPane>
+          <TabPane tab='Transaction Details' key='4'></TabPane>
+        </Tabs>
+      </Card>
+    </MainLayout>
+  );
 }
